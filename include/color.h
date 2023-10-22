2a3b04526eeb75035c3cece4c9dd323051028ada
meersuri <meersuri@gmail.com>
2023-10-22 08:40:20 -0400

    Add gamma correction to image

--- include/color.h
+++ include/color.h
@@ -5,11 +5,15 @@
 
 #include "vec3.h"
 
+inline double linear_to_gamma(double val) {
+    return sqrt(val);
+}
+
 void write_color(std::ostream& os, color pixel_color, int samples) {
     double scale = 1.0 / samples;
-    auto r = pixel_color.x() * scale;
-    auto g = pixel_color.y() * scale;
-    auto b = pixel_color.z() * scale;
+    auto r = linear_to_gamma(pixel_color.x() * scale);
+    auto g = linear_to_gamma(pixel_color.y() * scale);
+    auto b = linear_to_gamma(pixel_color.z() * scale);
     static const interval intensity(0, 0.999);
     os << static_cast<int>(256*intensity.clamp(r)) << ' '
         << static_cast<int>(256*intensity.clamp(g)) << ' '
